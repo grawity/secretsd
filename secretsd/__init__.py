34abd6cff4b2b40643c976aa485265f33a25123a
Mantas Mikulėnas <grawity@gmail.com>
2020-01-01 18:18:21 +0200

    extract BusObjectWithProperties, etc.

--- secretsd/__init__.py
+++ secretsd/__init__.py
@@ -12,65 +12,7 @@ import xdg.BaseDirectory
 from .database import SecretsDatabase
 from .exception import *
 from .session import SecretServiceSession
-
-NullObject = dbus.ObjectPath("/")
-
-def encode_path_component(value):
-    return "".join([c if c.isalnum() else "_%02x" % ord(c) for c in value])
-
-class Counter():
-    def __init__(self, start):
-        self.value = start
-
-    def increment(self):
-        v = self.value
-        self.value += 1
-        return v
-
-class BusObjectWithProperties():
-    PROPERTIES = {}
-
-    @dbus.service.method("org.freedesktop.DBus.Properties", "ss", "v",
-                         path_keyword="path")
-    def Get(self, interface, property, path=None):
-        if interface == self.INTERFACE:
-            if property in self.PROPERTIES:
-                getter, setter, value = self.PROPERTIES[property]
-                if getter:
-                    value = getter(self, path)
-                return value
-            else:
-                raise InvalidArgsException("No such property %r" % str(property))
-        else:
-            raise InvalidArgsException("No such interface %r" % str(interface))
-
-    @dbus.service.method("org.freedesktop.DBus.Properties", "s", "a{sv}",
-                         path_keyword="path")
-    def GetAll(self, interface, path=None):
-        if interface == self.INTERFACE:
-            out = {}
-            for name, (getter, setter, value) in self.PROPERTIES.items():
-                if getter:
-                    value = getter(self, path)
-                out[name] = value
-            return out
-        else:
-            raise InvalidArgsException("No such interface %r" % str(interface))
-
-    @dbus.service.method("org.freedesktop.DBus.Properties", "ssv", "",
-                         path_keyword="path")
-    def Set(self, interface, property, value, path=None):
-        if interface == self.INTERFACE:
-            if property in self.PROPERTIES:
-                getter, setter, _ = self.PROPERTIES[property]
-                if setter:
-                    setter(self, path, value)
-                else:
-                    raise InvalidArgsException("Property %r is read-only" % str(property))
-            else:
-                raise InvalidArgsException("No such property %r" % str(property))
-        else:
-            raise InvalidArgsException("No such interface %r" % str(interface))
+from .util import *
 
 class SecretServiceItemFallback(dbus.service.FallbackObject, BusObjectWithProperties):
     ROOT = "/org/freedesktop/secrets/item"
