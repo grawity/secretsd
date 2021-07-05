8d2e7366c7bdfa542057845b34bb5cf718cc3c6b
Mantas Mikulėnas <grawity@gmail.com>
2021-07-05 22:57:40 +0300

    load (or generate) the master key on startup
    This will be used as a separate thing from collection keys. If I ever
    get around implementing Collection.Unlock() to use per-collection
    password-derived keys, this will need to result in two encryption layers
    (first with the collection key at Secret API level, then with the master
    key at database level).

--- secretsd/__main__.py
+++ secretsd/__main__.py
@@ -1,3 +1,5 @@
+import argparse
+import base64
 import dbus
 import dbus.mainloop.glib
 from gi.repository import GLib
@@ -7,11 +9,29 @@ import xdg.BaseDirectory
 
 from .database import SecretsDatabase
 from .service import SecretService
+from .encryption import generate_key
+from .external_keys import load_ext_key, store_ext_key
 
 os.umask(0o077)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-k", "--master-key", metavar="PATH")
+args = parser.parse_args()
+
 db_dir = xdg.BaseDirectory.save_data_path("nullroute.eu.org/secretsd")
 db_path = os.path.join(db_dir, "secrets.db")
+key_path = os.path.join(db_dir, "secrets.key")
+mk_source = args.master_key or "file:%s" % key_path
+
+try:
+    mkey = base64.b64decode(load_ext_key(mk_source))
+except (KeyError, FileNotFoundError):
+    # No passphrases or interactive prompts -- we're a D-Bus service!
+    if os.path.exists(db_path):
+        exit("error: Unable to load master key from %r" % mk_source)
+    else:
+        mkey = generate_key()
+        store_ext_key(mk_source, base64.b64encode(mkey).decode())
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 bus = dbus.SessionBus()
---
+++ secretsd/encryption.py
@@ -0,0 +1,4 @@
+import os
+
+def generate_key():
+    return os.urandom(32)
