832c8f9d43439c8bb73485022409ad55f6a34a2a
Mantas Mikulėnas <grawity@gmail.com>
2021-07-06 13:59:51 +0300

    document the encryption capability

--- secretsd/__main__.py
+++ secretsd/__main__.py
@@ -12,13 +12,13 @@ from .service import SecretService
 os.umask(0o077)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-k", "--master-key", metavar="PATH")
+parser.add_argument("-k", "--key-location", metavar="PATH")
 args = parser.parse_args()
 
 db_dir = xdg.BaseDirectory.save_data_path("nullroute.eu.org/secretsd")
 db_path = os.path.join(db_dir, "secrets.db")
 key_path = os.path.join(db_dir, "secrets.key")
-key_path = args.master_key or "file:%s" % key_path
+key_path = args.key_location or "file:%s" % key_path
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 bus = dbus.SessionBus()
