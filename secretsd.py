44bcde0cc65209674d7bf91348ad1acd5ef25d81
Mantas Mikulėnas <grawity@gmail.com>
2021-07-06 12:24:57 +0300

    move main application from __init__ to __main__
    Apparently this is a better place, because it allows importing
    individual sub-modules for testing, as well as `py -m secretsd`.

--- secretsd.py
+++ secretsd.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python3
-import secretsd
+import secretsd.__main__
---
+++ secretsd/__main__.py
@@ -0,0 +1,22 @@
+import dbus
+import dbus.mainloop.glib
+from gi.repository import GLib
+import logging
+import os
+import xdg.BaseDirectory
+
+from .database import SecretsDatabase
+from .service import SecretService
+
+os.umask(0o077)
+
+db_dir = xdg.BaseDirectory.save_data_path("nullroute.eu.org/secretsd")
+db_path = os.path.join(db_dir, "secrets.db")
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+bus = dbus.SessionBus()
+sdb = SecretsDatabase(db_path)
+svc = SecretService(bus, sdb)
+
+loop = GLib.MainLoop()
+loop.run()
