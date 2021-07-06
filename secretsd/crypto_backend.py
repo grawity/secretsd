055c65668be22fd1d3008c58bc1e17ce6b06d47b
Mantas Mikulėnas <grawity@gmail.com>
2021-07-06 14:50:47 +0300

    move DH to crypto_backend

--- secretsd/crypto_backend.py
+++ secretsd/crypto_backend.py
@@ -6,10 +6,14 @@ __all__ = [
     "aes_cbc_decrypt",
     "aes_cfb8_encrypt",
     "aes_cfb8_decrypt",
+    "dh_modp1024_exchange",
     "hkdf_sha256_derive",
     "pkcs7_pad",
     "pkcs7_unpad",
 ]
+
+MODP1024_PRIME = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece65381ffffffffffffffff
+MODP1024_GENERATOR = 2
 
 backend = os.environ.get("CRYPTO_BACKEND", "cryptography")
 
@@ -17,6 +21,7 @@ if backend == "cryptodome":
     from Crypto.Cipher import AES
     from Crypto.Hash import SHA256
     from Crypto.Protocol.KDF import HKDF
+    from Crypto.Random.random import randint
     from Crypto.Util import Padding
 
     AES_BLOCK_BYTES = AES.block_size
@@ -33,6 +38,15 @@ if backend == "cryptodome":
     def aes_cfb8_decrypt(data, key, iv):
         return AES.new(key, AES.MODE_CFB, iv).decrypt(data)
 
+    def dh_modp1024_exchange(peer_pubkey):
+        prime = MODP1024_PRIME
+        generator = MODP1024_GENERATOR
+        our_privkey = randint(1, prime-1)
+        our_pubkey = pow(generator, our_privkey, prime)
+        shared_key = pow(peer_pubkey, our_privkey, prime)
+        shared_key = shared_key.to_bytes(1024 // 8, "big")
+        return our_pubkey, shared_key
+
     def hkdf_sha256_derive(input, nbytes):
         return HKDF(input, nbytes, b"", hashmod=SHA256)
 
@@ -42,7 +56,10 @@ if backend == "cryptodome":
     def pkcs7_unpad(data, size):
         return Padding.unpad(data, size, style="pkcs7")
 
+    print("using 'PyCryptodome' as crypto backend")
+
 elif backend == "cryptography":
+    from cryptography.hazmat.primitives.asymmetric import dh
     from cryptography.hazmat.primitives.ciphers import Cipher
     from cryptography.hazmat.primitives.ciphers.algorithms import AES
     from cryptography.hazmat.primitives.ciphers.modes import CBC, CFB8
@@ -51,6 +68,8 @@ elif backend == "cryptography":
     from cryptography.hazmat.primitives.padding import PKCS7
 
     AES_BLOCK_BYTES = AES.block_size // 8
+
+    MODP1024 = dh.DHParameterNumbers(p=MODP1024_PRIME, g=MODP1024_GENERATOR)
 
     def aes_cfb8_encrypt(data, key, iv):
         c = Cipher(AES(key), CFB8(iv)).encryptor()
@@ -68,6 +87,13 @@ elif backend == "cryptography":
         c = Cipher(AES(key), CBC(iv)).decryptor()
         return c.update(data) + c.finalize()
 
+    def dh_modp1024_exchange(peer_pubkey):
+        our_priv = MODP1024.parameters().generate_private_key()
+        peer_pub = dh.DHPublicNumbers(peer_pubkey, MODP1024).public_key()
+        shared_key = our_priv.exchange(peer_pub)
+        our_pubkey = our_priv.public_key().public_numbers().y
+        return our_pubkey, shared_key
+
     def hkdf_sha256_derive(input, nbytes):
         k = HKDF(algorithm=SHA256(),
                  length=nbytes,
@@ -82,6 +108,8 @@ elif backend == "cryptography":
     def pkcs7_unpad(data, size):
         p = PKCS7(size * 8).unpadder()
         return p.update(data) + p.finalize()
+
+    print("using 'python-cryptography' as crypto backend")
 
 else:
     raise RuntimeError("unsupported crypto backend %r" % backend)
--- secretsd/session.py
+++ secretsd/session.py
@@ -1,5 +1,3 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import dh
 import dbus
 import dbus.service
 import os
@@ -8,15 +6,11 @@ from .crypto_backend import (
     AES_BLOCK_BYTES,
     aes_cbc_encrypt,
     aes_cbc_decrypt,
+    dh_modp1024_exchange,
     hkdf_sha256_derive,
     pkcs7_pad,
     pkcs7_unpad,
 )
-
-MODP1024_PRIME=0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece65381ffffffffffffffff
-MODP1024_GEN=2
-
-MODP1024 = dh.DHParameterNumbers(p=MODP1024_PRIME, g=MODP1024_GEN)
 
 class SecretServiceSession(dbus.service.Object):
     ROOT = "/org/freedesktop/secrets/session"
@@ -33,15 +27,11 @@ class SecretServiceSession(dbus.service.Object):
         if self.algorithm == "plain":
             return (dbus.ByteArray(b""), True)
         elif self.algorithm == "dh-ietf1024-sha256-aes128-cbc-pkcs7":
-            dh_params = MODP1024.parameters(default_backend())
-            our_priv = dh_params.generate_private_key()
-            peer_pubn = dh.DHPublicNumbers(int.from_bytes(input, "big"),
-                                           MODP1024)
-            peer_pub = peer_pubn.public_key(default_backend())
-            shared_key = our_priv.exchange(peer_pub)
+            peer_pubkey = int.from_bytes(input, "big")
+            our_pubkey, shared_key = dh_modp1024_exchange(peer_pubkey)
+            output = our_pubkey.to_bytes(128, "big")
             self.crypt_key = hkdf_sha256_derive(shared_key, 128 // 8)
-            output = our_priv.public_key().public_numbers().y
-            return (dbus.ByteArray(output.to_bytes(128, "big")), True)
+            return (dbus.ByteArray(output), True)
         else:
             raise dbus.DBusException("org.freedesktop.DBus.Error.NotSupported")
 
