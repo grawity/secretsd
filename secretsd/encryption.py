7c46cd0436124ee6e9c2732185737a3dd3cf9c79
Mantas Mikulėnas <grawity@gmail.com>
2021-08-15 02:01:59 +0300

    enc: add aes_cfb128_(un)wrap

--- secretsd/encryption.py
+++ secretsd/encryption.py
@@ -6,6 +6,8 @@ from .crypto_backend import (
     AES_BLOCK_BYTES,
     aes_cfb8_encrypt,
     aes_cfb8_decrypt,
+    aes_cfb128_encrypt,
+    aes_cfb128_decrypt,
 )
 
 KEY_SIZE_BYTES = 32
@@ -38,3 +40,16 @@ def aes_cfb8_unwrap(buf, key):
         raise IOError("MAC verification failed")
     iv, ct = buf[:AES_BLOCK_BYTES], buf[AES_BLOCK_BYTES:]
     return aes_cfb8_decrypt(ct, key, iv)
+
+def aes_cfb128_wrap(data, key):
+    iv = os.urandom(AES_BLOCK_BYTES)
+    ct = aes_cfb128_encrypt(data, key, iv)
+    buf = iv + ct
+    return sha256_hmac(buf, key) + buf
+
+def aes_cfb128_unwrap(buf, key):
+    mac, buf = buf[:SHA256_HMAC_BYTES], buf[SHA256_HMAC_BYTES:]
+    if sha256_hmac(buf, key) != mac:
+        raise IOError("MAC verification failed")
+    iv, ct = buf[:AES_BLOCK_BYTES], buf[AES_BLOCK_BYTES:]
+    return aes_cfb128_decrypt(ct, key, iv)
